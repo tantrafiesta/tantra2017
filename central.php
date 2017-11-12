999e28971dbd1087e0547cefa71c73bc81f652e6
tantrafiesta <31889293+tantrafiesta@users.noreply.github.com>
2017-11-12 16:59:18 +0530

    Add files via upload

---
+++ central.php
@@ -0,0 +1,151 @@
+{
+"error":false,
+"central":[
+{
+  "clgname":"Indian Institute of Technology, Bombay",
+  "type":"IIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"578"
+},
+{
+  "clgname":"Indian Institute of Technology, Delhi",
+  "type":"IIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"DL",
+  "cutoff":"1278"
+},
+{
+  "clgname":"Indian Institute of Technology, Bombay",
+  "type":"IIT",
+  "branch":"Mechanical",
+  "bcode":"Mech",
+  "state":"MH",
+  "cutoff":"900"
+},
+{
+  "clgname":"Indian Institute of Technology, Kharagpur",
+  "type":"IIT",
+  "branch":"Mechanical",
+  "bcode":"Mech",
+  "state":"WB",
+  "cutoff":"900"
+},
+{
+  "clgname":"Indian Institute of Technology, Kanpur",
+  "type":"IIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"UP",
+  "cutoff":"1600"
+},
+{
+  "clgname":"Indian Institute of Technology, Bombay",
+  "type":"IIT",
+  "branch":"Electronics",
+  "bcode":"Electronics",
+  "state":"MH",
+  "cutoff":"1602"
+},
+{
+  "clgname":"Indian Institute of Technology, Delhi",
+  "type":"IIT",
+  "branch":"Electronics",
+  "bcode":"Electronics",
+  "state":"DL",
+  "cutoff":"1800"
+},
+{
+  "clgname":"Indian Institute of Technology, Kanpur",
+  "type":"IIT",
+  "branch":"Civil",
+  "bcode":"Civil",
+  "state":"UP",
+  "cutoff":"1883"
+},
+{
+  "clgname":"Indian Institute of Technology, Hyderabad",
+  "type":"IIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"TS",
+  "cutoff":"2200"
+},
+{
+  "clgname":"Indian Institute of Technology, Roorkee",
+  "type":"IIT",
+  "branch":"Electrical",
+  "bcode":"Electrical",
+  "state":"UK",
+  "cutoff":"2500"
+},
+{
+  "clgname":"Indian Institute of Technology, Roorkee",
+  "type":"IIT",
+  "branch":"Electrical",
+  "bcode":"Electrical",
+  "state":"UK",
+  "cutoff":"2500"
+},
+{
+  "clgname":"National Institute Technology, Raipur",
+  "type":"NIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"CG",
+  "cutoff":"2000"
+},
+{
+  "clgname":"National Institute Technology, Warangal",
+  "type":"NIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"TS",
+  "cutoff":"1200"
+},
+{
+  "clgname":"National Institute Technology, Warangal",
+  "type":"NIT",
+  "branch":"Electrical",
+  "bcode":"Electrical",
+  "state":"TS",
+  "cutoff":"1500"
+},
+{
+  "clgname":"Visvesarya National Institute Technology, Nagpur",
+  "type":"NIT",
+  "branch":"Electronics",
+  "bcode":"Electrical",
+  "state":"MH",
+  "cutoff":"2200"
+},
+{
+  "clgname":"Visvesarya National Institute Technology, Nagpur",
+  "type":"NIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"1800"
+},
+{
+  "clgname":"Indian Institute of Information Technology, Nagpur",
+  "type":"IIIT",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"3500"
+},
+{
+  "clgname":"Indian Institute of Information Technology, Nagpur",
+  "type":"IIIT",
+  "branch":"Electronics And Communication",
+  "bcode":"Electronics",
+  "state":"MH",
+  "cutoff":"4500"
+}
+
+]
+
+}
---
+++ state.php
@@ -0,0 +1,47 @@
+{
+"error":false,
+"state":[
+{
+  "clgname":"College of Engineering, Pune",
+  "type":"State",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"78"
+},
+{
+  "clgname":"VJTI, Mumbai",
+  "type":"State",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"258"
+},
+{
+  "clgname":"RCOEM, Nagpur",
+  "type":"State",
+  "branch":"Mechanical",
+  "bcode":"Mech",
+  "state":"MH",
+  "cutoff":"600"
+},
+{
+  "clgname":"YCCE, Nagpur",
+  "type":"State",
+  "branch":"Mechanical",
+  "bcode":"Mech",
+  "state":"MH",
+  "cutoff":"900"
+},
+{
+  "clgname":"GH Raisoni College of Engineering, Nagpur",
+  "type":"State",
+  "branch":"Computer Science",
+  "bcode":"ComputerScience",
+  "state":"MH",
+  "cutoff":"2000"
+}
+
+]
+
+}
